dbfe22318317c1592fc26306ad50ded09d57fe85
Qasim Hassan <qas_hassan@live.co.uk>
2016-06-30 16:36:22 +0100

    Initial Commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './application'
+require 'pry'
+require 'watir-webdriver'
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
