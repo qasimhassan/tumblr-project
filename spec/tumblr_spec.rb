8dd9ad322bbf89093486e64df9d353168001f523
Qasim Hassan <qas_hassan@live.co.uk>
2016-07-01 11:54:41 +0100

    Added scripts for tumblr post function

--- spec/tumblr_spec.rb
+++ spec/tumblr_spec.rb
@@ -11,20 +11,27 @@ describe Tumblr do
         login_btn.click
       end
       it "should find the email and password textfield and enter values then click login" do
-        email = @browser.div(:id, "signup_determine").text_field.value = "qas_hassan@live.co.uk"
+        email_elm = @browser.div(:id, "signup_determine").text_field.value = "qas_hassan@live.co.uk"
         next_btn = @browser.button id: "signup_forms_submit"
         next_btn.click
-        pass = @browser.text_field(id: "signup_password").set "godzilla1"
+        pass_elm = @browser.div(:id, "signup_account").text_field(:id, "signup_password")
+        pass_elm.when_present.set 'godzilla1'
         next_btn.click
       end
   end
 
   context 'create post tumblr tests' do
     it "should find the create post button" do
-      post_btn = @browser.button class: "compose-button"
-      post_btn.flash
+      text_btn = @browser.nav(:id, "post_buttons").a(:id, "new_post_label_text").click
+      title_elm = @browser.div(:class, "editor-plaintext").when_present
+      title_elm.click
+      title_elm.send_keys("test post")
+      body_elm = @browser.div(:class, "editor editor-richtext").when_present
+      body_elm.click
+      body_elm.send_keys("this is a test post")
+      post_btn = @browser.button(:class, "button-area create_post_button")
       post_btn.click
-      #post-type-icon icon-text
+#binding.pry
     end
   end
 end
